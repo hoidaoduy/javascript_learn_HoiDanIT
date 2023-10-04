6088a07d0e0b28d198b4a7f8045c33c08b10332e
hoidaoduy <hoidaoduy@gmail.com>
2023-10-04 15:24:13 +0700

    source video 5

---
+++ main.js
@@ -0,0 +1,17 @@
+var fullName = "Dao Duy Hoi";
+alert(fullName)
+console.log(123)
+
+setTimeout(function() {
+    alert('Thong bao')
+}, 1000)
+
+// setInterval(function() {
+//     console.log('Day la log' + Math.random())
+// }, 2000)
+/*
+Giới thiệu một số hàm built-in cơ bản:
+    1. alert (chuông báo) hiện lên thông báo để nhấp OK
+    2. console Gồm có console.log(), console.warn(), console.error(), ... để đẩy ra dòng thông tin ở console
+
+*/
